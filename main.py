9e28a9b3eddc5d7d6ed70331409711ff9c0ade8d
Tetenkov Alexander <forthe@ya.ru>
2024-10-09 20:07:16 +0400

    add mafia

--- main.py
+++ main.py
@@ -14,4 +14,16 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-    
+roles = ['p', 'p', 'm', 'm']
+roles_count = len(roles)
+
+@app.get("/role/")
+def read_games():
+    print(roles)
+    return roles.pop(random.randrange(len(roles))), roles_count
+
+@app.post("/roles/{mafia_count}/{peace_count}")
+def set_roles(mafia_count: int, peace_count: int):
+    roles = 'm' * mafia_count + 'p' * peace_count
+    roles_count = len(roles)
+    return 'ok'
